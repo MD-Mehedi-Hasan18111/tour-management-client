663690c9628243bc67fb5220e767eaa748004a85
mismailtamzid <mismailtamzid@gmail.com>
2023-11-14 22:27:26 +0600

    about page modified

--- src/global/components/UI/About/AboutExperience.jsx
+++ src/global/components/UI/About/AboutExperience.jsx
@@ -3,28 +3,30 @@ import img from "../../../../../public/images/bg.png";
 const AboutExperience = () => {
   return (
     <div
-      class="w-full bg-no-repeat bg-center bg-screen md:my-24 my-4"
+      class="w-full bg-no-repeat bg-center bg-screen md:my-10 my-4"
       style={{
         backgroundImage: `url(${img.src})`,
       }}
     >
       <div class="md:flex md:flex-row flex-col md:gap-y-16 gap-y-0 md:gap-6">
-        <div class="md:basis-1/2 w-full my-auto md:p-32 p-4 md:text-start text-center ">
-          <h2 className="text-4xl font-semibold">Our Experience</h2>
-          <h2 className="md:text-3xl text-2xl font-bold">
-            We make it easier for everyone to experience the world
-          </h2>
-          <h5 className="text-xl font-bold md:mt-0 mt-4">
-            Take your longest vacation yet!
-          </h5>
-          <p className="text-gray-500">
-            Travelling to all corners of the world is getting easier and easier.
-            We live in a global village, but how well do we know and understand
-            each other? Here is a simple test. Imagine you have arranged a
-            meeting at 4 p.m . What time should you expect your foreign business
-            colleagues to arrive?
-          </p>
-          <div className="flex md:flex-row flex-col justify-between my-16">
+        <div class="md:basis-1/2 w-full my-auto md:text-start text-center">
+          <div className=" md:px-32 md:pt-32 md:pb-16 px-4 pt-4 pb-4">
+            <h2 className="text-4xl font-semibold">Our Experience</h2>
+            <h2 className="md:text-3xl text-2xl font-bold">
+              We make it easier for everyone to experience the world
+            </h2>
+            <h5 className="text-xl font-bold md:mt-0 mt-4">
+              Take your longest vacation yet!
+            </h5>
+            <p className="text-gray-500">
+              Travelling to all corners of the world is getting easier and
+              easier. We live in a global village, but how well do we know and
+              understand each other? Here is a simple test. Imagine you have
+              arranged a meeting at 4 p.m . What time should you expect your
+              foreign business colleagues to arrive?
+            </p>
+          </div>
+          <div className="flex md:flex-row flex-col justify-around mb-16">
             <div className="text-center">
               <p className="font-bold md:text-2xl text-4xl">
                 Happy <br /> customer
