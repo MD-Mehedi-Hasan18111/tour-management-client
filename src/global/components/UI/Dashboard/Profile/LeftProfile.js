37cea23f116047abb4e2e3eb698bc362af4455f1
mismailtamzid <mismailtamzid@gmail.com>
2023-11-26 08:31:44 +0600

    admin propile info intigrated

--- src/global/components/UI/Dashboard/Profile/LeftProfile.js
+++ src/global/components/UI/Dashboard/Profile/LeftProfile.js
@@ -1,7 +1,92 @@
-import React from "react";
+import Profile from "@/pages/dashboard/admin/profile";
+import React, { useRef, useState } from "react";
 
 const LeftProfile = () => {
-  return <div>left profile</div>;
+  const inputRef = useRef(null);
+  const [image, setImage] = useState("");
+  const handleImageClick = () => {
+    inputRef.current.click();
+  };
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    const imgname = event.target.files[0].name;
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      const img = new Image();
+      img.src = reader.result;
+      img.onload = () => {
+        const canvas = document.createElement("canvas");
+        const maxSize = Math.max(img.width, img.height);
+        canvas.width = maxSize;
+        canvas.height = maxSize;
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(
+          img,
+          (maxSize - img.width) / 2,
+          (maxSize - img.height) / 2
+        );
+        canvas.toBlob((blob) => {
+          const file = new File([blob], imgname, {
+            type: "image/png",
+            lastModified: Date.now(),
+          });
+          console.log(file);
+        });
+      };
+    };
+
+    const handleUploadButtonClick = (file) => {
+      var myHeaders = new Headers();
+      const token = "allafsljkfhskcshfsflskgsdghsfd";
+      myHeaders.append("Authorization", `Bearer ${token}`);
+      var formdata = new FormData();
+      formdata.append("file", file);
+      var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: formdata,
+        redirect: "follow",
+      };
+    };
+
+    fetch("http:trickuweb.com/upload/profile_pic", requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        console.log(JSON.parse(result));
+        const profileurl = JSON.parse(result);
+        setImage(profileurl.img_url);
+      })
+      .catch((error) => console.log("error", error));
+  };
+  return (
+    <>
+      <div className="image-upload-container">
+        <div className="box-decoration">
+          <label htmlFor="image-upload-input" className="image-upload-label">
+            {image ? image.name : "Choose an image"}
+          </label>
+          <div onClick={handleImageClick} style={{ cursor: "pointer" }}>
+            {image ? (
+              <img src={URL.createObjectURL(image)} alt="" />
+            ) : (
+              <img
+                src="https://th.bing.com/th?id=OIP.j1rpn-KTUmqcVBQygXuOuQHaIk&w=232&h=268&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+                alt=""
+              />
+            )}
+            <input
+              type="file"
+              ref={inputRef}
+              onChange={handleImageChange}
+              style={{ display: "none" }}
+            />
+          </div>
+          <button className="image-upload-button">upload</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LeftProfile;
--- src/pages/dashboard/admin/profile.js
+++ src/pages/dashboard/admin/profile.js
@@ -1,8 +1,11 @@
 import EditProfile from "@/global/components/Modal-Body/EditProfile";
+import LeftProfile from "@/global/components/UI/Dashboard/Profile/LeftProfile";
 import DashboardLayout from "@/global/layouts/DashboardLayout";
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const user = useSelector((state) => state.auth.user);
   // Log In Modal states
   const [isProfileEditModal, setIsProfileEditModal] = useState(false);
   const onCloseProfileModal = () => {
@@ -11,29 +14,104 @@ const Profile = () => {
 
   return (
     <DashboardLayout>
-      <div>
-        <h2>My Profile</h2>
-        <button onClick={() => setIsProfileEditModal(true)}>
-          Edit Profile
-        </button>
-
-        <dialog
-          open={isProfileEditModal}
-          onClose={onCloseProfileModal}
-          className="modal"
-        >
-          <div className="modal-box lg:w-8/12 md:w-7/12 w-11/12 max-w-2xl">
-            <EditProfile setIsProfileEditModal={setIsProfileEditModal} />
+      <>
+        <h2 className="text-center text-2xl font-bold  uppercase mt-6">
+          Account Information
+        </h2>
+        <div className="flex flex-row items-center my-4">
+          {/*===================================
+                    Left Profile
+           ================================*/}
+          <div className="basis-1/2 text-center ">
+            <LeftProfile />
           </div>
-          <form
-            method="dialog"
-            className="modal-backdrop"
-            style={{ background: "rgba(0, 0, 0, 0.6)" }}
-          >
-            <button>close</button>
-          </form>
-        </dialog>
-      </div>
+          {/*===================================
+                    Right Profile
+           ================================*/}
+          <div className="basis-2/3 ps-10 ">
+            <div>
+              <div className="text-xl">
+                <div className="flex flex-row my-4">
+                  <div className="basis-1/3 font-medium">Rule</div>
+                  <div className="basis-1/3 border-b-2 uppercase border-gray-200 ps-4">
+                    {user?.role}
+                  </div>
+                </div>
+                <div className="flex flex-row justify-start mx-auto my-4">
+                  <div className="basis-1/3 font-medium">User ID</div>
+                  <div className="basis-1/3 border-b-2 border-gray-200 ps-4">
+                    {user?._id}
+                  </div>
+                </div>
+                <div className="flex flex-row justify-start mx-auto my-4">
+                  <div className="basis-1/3 font-medium">First Name</div>
+                  <div className="basis-1/3 border-b-2 border-gray-200 ps-4">
+                    {user?.firstName}
+                  </div>
+                </div>
+                <div className="flex flex-row mx-auto my-4">
+                  <div className="basis-1/3 font-medium">Last Name</div>
+                  <div className="basis-1/3 border-b-2 border-gray-200 ps-4">
+                    {user?.lastName}
+                  </div>
+                </div>
+                <div className="flex flex-row mx-auto my-4">
+                  <div className="basis-1/3 font-medium">Email</div>
+                  <div className="basis-1/3 border-b-2 border-gray-200 ps-4">
+                    {user?.email}
+                  </div>
+                </div>
+                <div className="flex flex-row mx-auto my-4">
+                  <div className="basis-1/3 font-medium">Gender</div>
+                  <div className="basis-1/3 border-b-2 uppercase border-gray-200 ps-4">
+                    {user?.gender}
+                  </div>
+                </div>
+                <div className="flex flex-row mx-auto my-4">
+                  <div className="basis-1/3 font-medium">Contact Number</div>
+                  <div className="basis-1/3 border-b-2 border-gray-200 ps-4">
+                    {user?.mobileCode}
+                    {user?.phoneNumber}
+                  </div>
+                </div>
+                <div className="flex flex-row mx-auto my-4">
+                  <div className="basis-1/3 font-medium">Created At</div>
+                  <div className="basis-1/3 border-b-2 border-gray-200 ps-4">
+                    {user?.createdAt}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* =======================
+                 Update modal
+          ======================== */}
+            <button
+              className="my-10 lg:text-[16px] text-[14px] rounded-[10px] bg-blue-400 hover:bg-blue-300 text-white lg:w-[336px] w-[236px] lg:py-[16px] py-[12px] mx-auto font-[600]"
+              onClick={() => setIsProfileEditModal(true)}
+            >
+              Edit Profile
+            </button>
+
+            <dialog
+              open={isProfileEditModal}
+              onClose={onCloseProfileModal}
+              className="modal"
+            >
+              <div className="modal-box lg:w-8/12 md:w-7/12 w-11/12 max-w-2xl">
+                <EditProfile setIsProfileEditModal={setIsProfileEditModal} />
+              </div>
+              <form
+                method="dialog"
+                className="modal-backdrop"
+                style={{ background: "rgba(0, 0, 0, 0.6)" }}
+              >
+                <button>close</button>
+              </form>
+            </dialog>
+          </div>
+        </div>
+      </>
     </DashboardLayout>
   );
 };
